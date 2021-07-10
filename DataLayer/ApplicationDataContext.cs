626e7481d46a71eada38ae722c4095e2a1b66602
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-07-10 09:00:34 +0300

    Создание слоев. Настрока API. Настройка базы данных

---
+++ DataLayer/ApplicationDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
