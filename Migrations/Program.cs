9701be23afdf392914c5c43d50b7604b47075097
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-07-10 11:08:25 +0300

    Добавление миграций

--- Migrations/Program.cs
+++ Migrations/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Migrations
 {
@@ -6,7 +11,28 @@ namespace Migrations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddDbContext<ApplicationDataContext>(options =>
+                    {
+                        options.UseNpgsql(
+                            hostContext.Configuration.GetConnectionString("DefaultConnection"),
+                            b => b.MigrationsAssembly(nameof(Migrations))
+                        ).UseLoggerFactory(LoggerFactory.Create(builder =>
+                        {
+                            builder
+                                .AddConsole((_) => { })
+                                .AddFilter((category, level) =>
+                                    category == DbLoggerCategory.Database.Command.Name
+                                    && level == LogLevel.Information);
+                        }));
+                    });
+                    services.AddScoped<Worker>();
+                });
     }
 }
